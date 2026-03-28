793434a0f4a9a73429df684ea38ae48ebe35f774
aledifon <aledifon89@gmail.com>
2026-03-28 14:21:02 +0100

    new try-catch exercises

---
+++ js-basics/HelloWorld/js/37-try-catch.js
@@ -0,0 +1,15 @@
+'use strict'
+
+window.addEventListener('load', () => {
+
+    try
+    {
+        var year = 2019;
+        alert(year);
+        console.log(decodeURIComponent("https://dakg%messtudio.taplink.site/"));
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+});
