a0479bf608f7e932deb94ee84530a4df6330d197
aledifon <aledifon89@gmail.com>
2026-03-29 15:10:59 +0200

    Started with Effects & Animations exercises with jQuery

--- jQuery-Learning/js/03-texts.js
+++ jQuery-Learning/js/03-texts.js
@@ -1,6 +1,42 @@
 'use strict'
 
-$(document).ready(function() {    
-   
+$(document).ready(function() {       
+
+    // Vars
+    var a = $('a');
+    var addButton = $('#addButton');
+    var addLink = $('#addLink');
+    var list = $('#list');
+
+    // More compact sintaxis
+    function reloadLinks(){
+        a = $('a');                             // Update all the a refs.
+        a.each(function(idx){                   // jQuery foreach func.
+            let _this = $(this);        
+
+            _this.attr('target','_blank');      // Add the target attribute with the '_blank' value         
+
+            _this.text(_this.attr('href'));     // Set the 'href' value as the content of
+                                                //  each 'a' html elem.
+            });
+    }
+
+    // Longer sintaxis
+//    var elemList = $('li');   
+//    for(let idx in elemList) {
+//         elemList.eq(idx).text(elemList.eq(idx).find('[href]').eq(0).attr('href'));
+//         console.log(elemList.eq(idx).text());
+//    };   
+
+    reloadLinks();
     
+    // removeAttr --> Remove attributes;  
+    addButton
+        .removeAttr('disabled')
+        .click(function(){             
+        list.append('<li><a href="' + addLink.val() + '">Test</a></li>');        
+        reloadLinks();
+        addLink.val('');
+        });
+
 });
