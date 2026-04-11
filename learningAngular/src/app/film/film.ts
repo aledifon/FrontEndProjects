0dde3622ed59fe5ffe112227881f5419a1acc408
aledifon <aledifon89@gmail.com>
2026-04-11 17:43:57 +0200

    Starting samples of lyfe cycle components and life cycle hooks

--- learningAngular/src/app/film/film.ts
+++ learningAngular/src/app/film/film.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './film.html',
   styleUrl: './film.scss',
 })
-export class Film {}
+export class Film {
+
+  ngOnInit(){
+    console.log("The film component is running!!");
+  }
+
+  ngOnDestroy(){
+    console.log("The film component has been destroyed!!");
+  }
+}
--- learningAngular/src/app/product/product.component.ts
+++ learningAngular/src/app/product/product.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { Film } from '../film/film';
-import { Sellings } from '../sellings/sellings';
-import { User } from '../user/user';
 
 @Component({
     selector: "product",
-    imports: [Film, User, Sellings],
+    imports: [Film],
     templateUrl: "./product.component.html",
     styleUrl: "./product.component.scss"
 })
@@ -16,13 +14,39 @@ export class ProductComponent{
     public Price: number;
     public Description: string;
 
+    public showFilms: boolean = true;
+
     constructor(){
         this.Name = "Portable PC";
         this.Brand = "Asus";
         this.Price = 400;
-        this.Description = "This pc is really powerful";
+        this.Description = "This pc is really powerful";        
 
         console.log("The product component was loaded!!");        
     }
 
+    // Lifecycle Hooks
+
+    // Runs once after Angular has initialized all the component's inputs. 
+    ngOnInit(){
+       console.log("The component is initialized");
+    }
+
+    // 	Runs once after the component's view has been initialized.
+    ngAfterViewInit(){
+        console.log("The view is loaded");
+    }
+
+    // Runs every time this component is checked for changes.
+    ngDoCheck(){
+        console.log("Component updated");
+    }
+
+    changeName(){
+        this.Name = "Laptop asus wii";
+    }
+
+    hideFilms(value: boolean){
+        this.showFilms = value;
+    }
 }
